ad471ef0ea46d1231e0294a645f4385340ad8cf4
sgulrich <65795281+sgulrich@users.noreply.github.com>
2023-03-29 16:44:10 -0500

    Add Challenge Activity

--- app/src/main/java/com/example/campusguessr/ChallengeActivity.java
+++ app/src/main/java/com/example/campusguessr/ChallengeActivity.java
@@ -5,23 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
-public class StartChallengeActivity extends AppCompatActivity {
+public class ChallengeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.start_challenge);
-        Button StartChallengeButton = (Button) findViewById(R.id.start_challenge_button);
+        setContentView(R.layout.challenge);
         ImageButton RankingsButton = (ImageButton) findViewById(R.id.navigate_ranking_tab_button);
         ImageButton ProfileButton = (ImageButton) findViewById(R.id.navigate_profile_tab_button);
-        StartChallengeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), ChallengeActivity.class));
-            }
-        });
         RankingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
