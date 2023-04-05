d3bda818cfc0c39adb09a88ef9ec97677d2acdd2
Dongwon Nam <dongwonnam98@gmail.com>
2023-04-05 17:22:24 -0500

    Added activity for duplicate detect and verify photo

---
+++ app/src/main/java/com/example/campusguessr/DuplicateDetectActivity.java
@@ -0,0 +1,13 @@
+package com.example.campusguessr;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DuplicateDetectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.duplicate_detect);
+    }
+}
