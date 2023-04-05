623069f43b2fdaf7f04b66919a43dde8834e225d
Dongwon Nam <dongwonnam98@gmail.com>
2023-04-05 16:06:31 -0500

    added ui skeleton for duplicate detected and verify photo

--- app/src/main/java/com/example/campusguessr/MainActivity.java
+++ app/src/main/java/com/example/campusguessr/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(getApplicationContext(), StartChallengeActivity.class));
+        setContentView(R.layout.duplicate_detect);
+//        startActivity(new Intent(getApplicationContext(), StartChallengeActivity.class));
     }
 }
