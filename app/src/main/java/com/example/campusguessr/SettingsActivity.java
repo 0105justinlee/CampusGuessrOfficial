59f57da98d509c717d8b43325843336a8a07a90b
zberglund <zberg9@gmail.com>
2023-04-17 16:43:48 -0500

    Created references to sliders and username on settings page. Still need to send updated versions to database

--- app/src/main/java/com/example/campusguessr/SettingsActivity.java
+++ app/src/main/java/com/example/campusguessr/SettingsActivity.java
@@ -1,12 +1,18 @@
 package com.example.campusguessr;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.SeekBar;
 
 public class SettingsActivity extends AppCompatActivity {
   
@@ -18,6 +24,16 @@ public class SettingsActivity extends AppCompatActivity {
     ImageButton CreateButton = (ImageButton) findViewById(R.id.navigate_create_tab_button);
     ImageButton PlayButton = (ImageButton) findViewById(R.id.navigate_play_tab_button);
     ImageButton ProfileButton = (ImageButton) findViewById(R.id.navigate_profile_tab_button);
+  
+    EditText NewUsername = (EditText) findViewById(R.id.settings_edittext_username);
+    Button SubmitUsernameButton = (Button) findViewById(R.id.settings_username_button);
+    
+    SeekBar DesiredDistance = (SeekBar) findViewById(R.id.settings_seekbar_desiredDistance);
+    SeekBar DesiredDifficulty = (SeekBar) findViewById(R.id.settings_seekbar_desiredDifficulty);
+    // TODO set initial progress of seekbars by reading database
+    // DesiredDistance.setProgress()
+    // DesiredDifficulty.setProgress()
+    
   
     RankingsButton.setOnClickListener(new View.OnClickListener() {
       @Override
@@ -43,5 +59,56 @@ public class SettingsActivity extends AppCompatActivity {
         startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
       }
     });
+    
+    SubmitUsernameButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        // attempt to set new username
+        setUsername(String.valueOf(NewUsername.getText()));
+      }
+    });
+    DesiredDistance.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+      @Override
+      public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+    
+      }
+  
+      @Override
+      public void onStartTrackingTouch(SeekBar seekBar) {
+    
+      }
+  
+      @Override
+      public void onStopTrackingTouch(SeekBar seekBar) {
+    
+      }
+    });
+    DesiredDifficulty.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+  
+      @Override
+      public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        Log.d(TAG, "onProgressChanged: " + i + " " + b);
+      }
+  
+      @Override
+      public void onStartTrackingTouch(SeekBar seekBar) {
+        Log.d(TAG, "onStartTrackingTouch: ");
+      }
+  
+      @Override
+      public void onStopTrackingTouch(SeekBar seekBar) {
+        Log.d(TAG, "onStopTrackingTouch: ");
+        Log.d(TAG, "onStopTrackingTouch: " + seekBar.getProgress());
+        seekBar.setProgress(100);
+      }
+    });
+  }
+  
+  private void setUsername(String username) {
+    // ensure text does not contain invalid characters
+    
+    // ensure this username does not exist elsewhere in the database
+    
+    // set username
   }
 }
